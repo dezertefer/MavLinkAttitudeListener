4da5a75f326b466ca46820cfaaeae57c544a2ab7
Danila Drozdov <drozdovdanilsss@gmail.com>
2024-12-08 10:40:24 +0800

    Update generate_aruco_marker.py

--- generate_aruco_marker.py
+++ generate_aruco_marker.py
@@ -4,24 +4,26 @@ import argparse
 
 
 # Function to generate and save an ArUco marker
-def generate_aruco_marker(id, size=300, file_name="marker.jpg"):
+def generate_aruco_marker(ids, size=300):
     # Define the ArUco dictionary
     aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_1000)
-    
-    # Create an empty image to store the marker
-    marker_image = aruco.drawMarker(aruco_dict, id, size)
-    
-    # Save the marker image as a JPEG
-    cv2.imwrite(file_name, marker_image)
-    print(f"ArUco marker with ID {id} saved as {file_name}")
+
+    for i, id in enumerate(ids):
+        file_name = f'marker-{i + 1}.jpg'
+        # Create an empty image to store the marker
+        marker_image = aruco.drawMarker(aruco_dict, id, size)
+
+        # Save the marker image as a JPEG
+        cv2.imwrite(file_name, marker_image)
+        print(f"ArUco marker with ID {id} saved as {file_name}")
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Generate an ArUco marker.")
-    parser.add_argument("--id", type=int, required=True, help="ID of the ArUco marker.")
+    parser = argparse.ArgumentParser(description="Generate ArUco markers.")
+    parser.add_argument("--ids", type=int, nargs='+', required=True, help="ID array of the ArUco markers.")
 
     # Parse arguments
     args = parser.parse_args()
     
     # Example usage
-    generate_aruco_marker(id=args.id, size=300)  # Generates a marker with ID 1, size 300x300
+    generate_aruco_marker(ids=args.ids, size=300)  # Generates a marker with ID 1, size 300x300
